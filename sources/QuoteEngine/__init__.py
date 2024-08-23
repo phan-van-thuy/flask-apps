d6788a09384b5e8cbc9281ecb8c35ea5fa2de8e4
thuyphanv <thuyphanv@gmail.com>
2024-08-23 10:55:12 +0700

    edit app flas

--- sources/QuoteEngine/__init__.py
+++
@@ -1,6 +0,0 @@
-from .IngestorInterface import IngestorInterface
-from .TextIngestor import TextIngestor
-from .DocxIngestor import DocxIngestor
-from .PDFIngestor import PDFIngestor
-from .CSVIngestor import CSVIngestor
-from .QuoteModel import QuoteModel
